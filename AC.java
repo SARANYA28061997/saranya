d530b033d7e3b6c8dd7d14994692a4a716d039b5
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-06-08 18:33:41 +0530

    Create AC.java

---
+++ AC.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class AC
+{	
+public static void main(String args[])
+{
+char[] alphabets={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+ char l='s';
+int n=26;
+for(int i=0;i<n-1;i++)
+{
+if(l==alphabets[i])
+ {
+    System.out.print("alphabet");
+ }
+
+}
+}
+}
