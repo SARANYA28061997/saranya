38eedff91fd761ed442e4563161d3956654269b6
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-06-08 17:17:27 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class A
+{
+public static void main(String args[])
+{
+int n=Integer.parseInt(args [0]);
+if(n<0)
+{ 
+System.out.println("negetive number");
+}
+if(n==0)
+{
+System.out.println("zero");
+}
+if(n<0)
+{
+System.out.println("positive number");
+}
+}
+}
