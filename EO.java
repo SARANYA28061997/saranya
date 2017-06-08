c3cff763881476589bec81cbb4b602450b7fffe5
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-06-08 17:25:39 +0530

    Create EO.java

---
+++ EO.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class EO
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0]);
+int n2=Integer.parseInt(args[1]);
+if(n1%n2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
