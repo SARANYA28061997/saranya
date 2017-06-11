0452db466e0cdc25925696932f339cf1091956fd
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-06-11 21:38:26 +0530

    Create Repeat.java

---
+++ Repeat.java
@@ -0,0 +1,41 @@
+public class Repeat
+{
+
+
+ public static void main(String[] args)
+ {
+  int num=0,count=0;
+  System.out.println("Enter the number of elements  in array ");
+  Scanner scr=new Scanner(System.in);
+  int n=scr.nextInt();
+  int[] Arr=new int[n];
+  System.out.println("Enter the elements:");
+  for(int i=0;i<n;i++)
+   {
+     Arr[i]=scr.nextInt();
+    }
+  for(int i=0;i<n;i++)
+    { 
+      if(count==0)
+      {
+         for(int j=i+1;j<n;j++)
+         {
+           if(Arr[i]==(Arr[j]))
+            { 
+              num=Arr[i];
+              count+=1;
+              break;
+            }
+         }
+       }
+                
+      scr.close();
+      }
+  System.out.println("REPEATED ELEMENT:"+num); 
+ }
+} 
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2017 GitHub, Inc. Terms Privacy Security Status Help 
+
