e7a9ffabe32c3ccfc63f3b4449de846dcf774e15
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-06-21 10:14:40 +0530

    Create DAYS.java

---
+++ DAYS.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class DAYS
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.next();
+switch(str)
+{
+case "sunday":
+ System.out.println("false");
+ break;
+ case "monday":
+ System.out.println("true");
+ break;
+ case "tuesday":
+ System.out.println("true");
+ break;
+ case "wednesday":
+ System.out.println("true");
+ break;
+ case "thursday":
+ System.out.println("true");
+ break;
+ case "friday":
+ System.out.println("true");
+ break;
+ case "saturday":
+ System.out.println("true");
+ break;
+ }
+ }
+ }
