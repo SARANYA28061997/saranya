6d279b9f0881fbe68adfea7069a6dd19559e9ec6
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-07-07 15:00:53 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,18 @@
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n>0 && n<999)
+{
+ if(n%2==0)
+ {
+ System.out.println("even");
+ }
+ else
+ {
+ System.out.println("not even");
+ }
+ else
+ {
+ System.out.println("not in range");
+ }}
