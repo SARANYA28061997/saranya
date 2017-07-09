434354e2366e28993188032edad311ebea8360ea
SARANYA28061997 <saranyaks28.sk@gmail.com>
2017-07-09 12:03:29 +0530

    Create wrev.java

---
+++ wrev.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class wrev
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+String[] a=str.split(" ");
+int n=a.length;
+String temp="NULL";
+
+    for(int j=n-1;j>=0;j--)
+    {
+System.out.print(a[j]);
+}}
+}
